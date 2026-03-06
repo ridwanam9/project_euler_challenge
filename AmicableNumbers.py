d2fb8f796152e42ed75298b070a28d0a0485be11
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2026-03-06 20:52:33 +0700

    AmicableNumbers fixed

--- AmicableNumbers.py
+++ AmicableNumbers.py
@@ -1,46 +1,63 @@
 
 
-n = 7
-# m = 5
-print(f"n = {n}")
-print("-------------")
+# n = 7
+# # m = 5
+# print(f"n = {n}")
+# print("-------------")
 
-p = 3*(2**(n-1)) - 1
-print(p)
-q = 3*(2**n) - 1
-print(q)
-r = 9*(2**(2*n-1)) - 1
-print(r)
-
-
-# p = ((2^(n-m)) + 1) * (2^m) - 1
+# p = 3*(2**(n-1)) - 1
 # print(p)
-# q = ((2^(n-m)) + 1) * (2^n) - 1
+# q = 3*(2**n) - 1
 # print(q)
-# r = ((2^(n-m)) + 1) * (2^(m + n)) - 1
+# r = 9*(2**(2*n-1)) - 1
 # print(r)
 
-print("-------------")
 
-a = (2**n) * p * q
-print(a)
-b = (2**n) * r
-print(b)
+# # p = ((2^(n-m)) + 1) * (2^m) - 1
+# # print(p)
+# # q = ((2^(n-m)) + 1) * (2^n) - 1
+# # print(q)
+# # r = ((2^(n-m)) + 1) * (2^(m + n)) - 1
+# # print(r)
 
-print("-------------")
-sumA1 = 0
-print(f"{a} Divisors = ", end="")
-for j in range(1, a):
-    if a%j == 0:
-        print(j, end=" ")
-        sumA1 += j
-print(f"\n d({a}) = {sumA1}")
+# print("-------------")
 
-print("\n")
-sumA2 = 0
-print(f"{b} Divisors = ", end="")
-for x in range(1, b):
-    if b%x == 0:
-        print(x, end=" ")
-        sumA2 += x
-print(f"\n d({b}) = {sumA2}")
+# a = (2**n) * p * q
+# print(a)
+# b = (2**n) * r
+# print(b)
+
+# print("-------------")
+# sumA1 = 0
+# print(f"{a} Divisors = ", end="")
+# for j in range(1, a):
+#     if a%j == 0:
+#         print(j, end=" ")
+#         sumA1 += j
+# print(f"\n d({a}) = {sumA1}")
+
+# print("\n")
+# sumA2 = 0
+# print(f"{b} Divisors = ", end="")
+# for x in range(1, b):
+#     if b%x == 0:
+#         print(x, end=" ")
+#         sumA2 += x
+# print(f"\n d({b}) = {sumA2}")
+
+
+LIMIT = 10000
+div_sum = [0] * LIMIT
+
+for i in range(1, LIMIT):
+    for j in range(i*2, LIMIT, i):
+        div_sum[j] += i
+
+amicable_sum = 0
+
+for i in range(1, LIMIT):
+    partner = div_sum[i]
+    if partner < LIMIT and partner != i and div_sum[partner] == i:
+        amicable_sum += i
+
+print(amicable_sum)
